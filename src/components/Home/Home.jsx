de24e42efb75e7a8decf9354e9992d6b58cee72b
pooja-thalur <poojathalur@yahoo.com>
2025-07-21 10:42:21 -0700

    css change

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -5,7 +5,7 @@ function Home() {
   return (
     <div className="home-container">
       <h1 className='home-title'>Welcome to the Photo Gallery Explorer</h1>
-      <Link to="/gallery"><button>Go to the Gallery</button></Link>
+      <Link to="/gallery"><button className='home-button'>Go to the Gallery</button></Link>
     </div>
   );
 }
